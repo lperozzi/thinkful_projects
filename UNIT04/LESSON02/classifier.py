b41afa3d05a3e8d72a274be8baa6d56d1c0dc082
Lorenzo Perozzi <lorenzo.perozzi@gmail.com>
2015-08-20 15:13:31 -0400

    classifier (wrong solution)

--- UNIT04/LESSON02/classifier.py
+++ UNIT04/LESSON02/classifier.py
@@ -7,6 +7,8 @@ Created on Wed Aug 19 19:42:55 2015
 from commands import getstatusoutput as system
 import numpy as np
 import pandas as pd
+from nltk.corpus import stopwords
+import re
 
 # Count files in folder
 folder = "data/enron1"
@@ -21,12 +23,52 @@ P_spam = spam_count / (spam_count + ham_count)
 P_ham = ham_count / (spam_count + ham_count)
 
 df_spam = pd.read_csv('data/spam_vocabulary.csv',names=['word', 'frequency'])
-df_spam['probailities'] = df_spam['frequency'] / spam_count
+df_ham = pd.read_csv('data/ham_vocabulary.csv', names=['word','frequency'])
 
-P_word_given_spam = np.zeros(len(word1))
-P_word_given_ham = np.zeros(len(word1))
-for i in range(len(word1)):
-    # Compute the conditional probabilities of word given spam
-    P_word_given_spam[i] = word_in_spam_count[i] / spam_count
-    # Compute the conditional probabilities of word given ham
-    P_word_given_ham[i] = word_in_ham_count[i] / ham_count
+# calculate theta (probability that an individual word is present in a ham email)
+df_ham['theta'] = df_ham['frequency'] / (spam_count + ham_count)
+# calculate theta (probability that an individual word is present in a spam email)
+df_spam['theta'] = df_spam['frequency'] / (spam_count + ham_count)
+# calculate wj (log(theta/(1-theta)))
+df_spam['wj'] = np.log(df_spam['theta'] / (1 - df_spam['theta']))
+# calculate log(1-theta) 
+df_spam['log(1-theta)'] = np.log(1 - df_spam['theta'])
+
+# calculate xi (email vector for those entries are 1 or 0) 
+folder = 'data/enron1/spam/'
+f = open(folder+'0008.2003-12-18.GP.spam.txt')
+raw = f.read()
+letters_only = re.sub("[^a-zA-Z]"," ",raw)
+lower_case = letters_only.lower()
+words = lower_case.split()            
+words = [w for w in words if w not in stopwords.words("english")]
+# find words that are in spam vocabulary
+words = '|'.join(words)
+df_spam['xi'] = df_spam['word'].str.contains(words)
+df_spam['xi'] = df_spam['xi'].astype('bool')
+# convert xi column in 0 or 1 value
+df_spam['xi'] = df_spam['xi'].astype(int)
+
+# it xi must be multiplied with the theta?
+#df_spam['xi_2'] = df_spam['theta'] * df_spam['xi']
+
+# calculate w0
+w0 = df_spam['log(1-theta)'].sum()
+# calculate (xi * wj)
+df_spam['xi*wj'] = df_spam['wj'] * df_spam['xi'] 
+
+
+
+# calculate sum (xi*wj)
+a = df_spam['xi_2*wj'].sum()
+
+#b = a + w0
+
+#b * np.log(P_spam) / (np.log(df_spam['theta'] + df_ham['theta']))
+
+
+
+
+
+
+
